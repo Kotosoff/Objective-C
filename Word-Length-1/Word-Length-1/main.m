5c8d1a77c6f4897c2cd07022513e54cbf8e6eff5
Sean Jones <decode.icarus@gmail.com>
2011-08-25 12:00:49 +0100

    Added support for words in a txt file instead of an array

--- Word-Length-1/Word-Length-1/main.m
+++ Word-Length-1/Word-Length-1/main.m
@@ -10,13 +10,17 @@
 
 int main (int argc, const char * argv[])
 {
-    const char *words[4] = { "Joe-Bob \"Handyman\" Brown", "Jacksonville \"Sly\" Murphy", "Shinara Bain", "George \"Guitar\" Books" };
-    int wordCount = 4;
-    
-    int i;
-    for (i = 0; i < wordCount; i++) {
-        NSLog(@"%s is %d characters long", words[i], strlen(words[i]));
+    FILE *wordFile = fopen ("/tmp/words.txt", "r");
+    char word[100];
+   
+    while (fgets(word, 100, wordFile)) {
+        // strip off the trailing \n
+        word[strlen(word) - 1] = '\0';
+
+        NSLog(@"%s is %d characters long", word, strlen(word));
     }
+    
+    fclose (wordFile);
     
     return (0);
 } // main
