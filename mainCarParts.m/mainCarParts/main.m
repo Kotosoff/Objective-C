9bd693370736401a4a2d98ddbe784a1496ddb685
Sean Jones <decode.icarus@gmail.com>
2011-08-25 17:56:13 +0100

    First commit my car has an engine and 4 tires Vrrom vrroom!

---
+++ mainCarParts.m/mainCarParts/main.m
@@ -0,0 +1,79 @@
+//
+//  main.m
+//  mainCarParts
+//
+//  Created by Sean Jones on 25/08/2011.
+//  Copyright 2011 BrainPicnic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tire : NSObject 
+@end // Tire
+@implementation Tire
+
+- (NSString *) description
+{
+    return (@"I am a tire. I last a while");
+} // description
+
+@end // Tire
+
+@interface Engine : NSObject
+@end
+@implementation Engine
+
+- (NSString *) description
+{
+    return (@"I am an engine. Vrooom!");
+} // descripton
+
+@end // Engine
+
+@interface Car : NSObject {
+    Engine *engine;
+    Tire *tires[4];
+}
+
+- (void) print;
+
+@end // Car
+
+@implementation Car
+
+- (id) init
+{
+    if (self = [super init]) {
+        engine = [Engine new];
+        
+        tires[0] = [Tire new];
+        tires[1] = [Tire new];
+        tires[2] = [Tire new];
+        tires[3] = [Tire new];
+    }
+    
+    return (self);
+    
+} // init
+
+- (void) print
+{
+    NSLog(@"%@", engine);
+    
+    NSLog(@"%@", tires[0]);
+    NSLog(@"%@", tires[1]);
+    NSLog(@"%@", tires[2]);
+    NSLog(@"%@", tires[3]);
+} // print
+
+@end // Car
+
+int main (int argc, const char * argv[])
+{
+    Car *car;
+    
+    car = [Car new];
+    [car print];
+   
+    return (0);
+} // main
